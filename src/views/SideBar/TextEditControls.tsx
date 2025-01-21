b3e48b580583b1c99dad0ace2d80c44bb2aabfcf
VladimirB312 <v.r@inbox.ru>
2025-01-21 16:27:00 +0300

    добавил размер шрифта

--- src/views/SideBar/TextEditControls.tsx
+++ src/views/SideBar/TextEditControls.tsx
@@ -75,6 +75,7 @@ const TextEditControls = ({slide, selectedElementId}: TextEditControlsProps) => 
                     <option value='36px'>36px</option>
                     <option value='48px'>48px</option>
                     <option value='60px'>60px</option>
+                    <option value='72px'>72px</option>
                 </select>
             </form>
             <div className={classes.colorPickerWrapper}>
